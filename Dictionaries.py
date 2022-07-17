c64f9e909205d8ab8f2e8c108309f69f691d60ec
Kirk Cedrick Cartaño <kirkcedrickscartano@iskolarngbayan.pup.edu.ph>
2022-07-17 21:34:09 +0800

    initial commit

---
+++ Dictionaries.py
@@ -0,0 +1,40 @@
+# Basic dictionary
+shopping_list = {"weird hat": 150, "purple_rug": 450,
+                 "old_books": 200, "stuffed_elephant": 50}
+print(shopping_list)
+
+# List in dictionary
+basketball_team = {"Crouching_tigers": [
+    "Jacob", "Kevin", "Roni", "Joshua", "Isagani"]}
+print(basketball_team)
+
+empty_dictionary = {}
+print(empty_dictionary)
+
+# Add a new key-value pairs for 1 pair only
+shopping_list["cellphone"] = 8700
+print(shopping_list)
+
+# Overwrite a key-value pairs
+shopping_list["weird hat"] = 200
+print(shopping_list)
+
+# Update method to add more key-value pairs
+shopping_list.update({"Cap": 200, "Cellphone case": 160})
+print(shopping_list)
+
+# zip function combines two or more list into a single list
+relative = {"Aunt Mart", "Jeff", "Aunt John", "Veronica"}
+age = [54, 14, 55, 19]
+
+print(relative, age)
+
+combined_list = zip(relative, age)
+print(list(combined_list))
+
+# dict function turns combined list into a dictionary
+relative_age = dict(zip(relative, age))
+print(relative_age)
+
+# To get a single value of a key
+print(shopping_list.get("weird hat"))
