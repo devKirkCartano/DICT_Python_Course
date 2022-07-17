8e570496b96cd86fc1e6696b3088335cc7c9da88
Kirk Cedrick Cartaño <kirkcedrickscartano@iskolarngbayan.pup.edu.ph>
2022-07-17 21:50:33 +0800

    initial commit

---
+++ CodingExercise1.9.py
@@ -0,0 +1,15 @@
+flavors = ["vanilla", "chocolate", "strawberry",
+           "cookies n' cream", "bubblegum"]
+toppings = ["almonds", "banana slices", "chocolate syrup",
+            "caramel syrup", "white chocolate chips"]
+
+# combine list
+
+ice_cream = dict(zip(flavors, toppings))
+print(ice_cream)
+
+ice_cream["chocolate"] = "blueberries"
+print(ice_cream)
+
+ice_cream.update({"matcha": "pistachios", "ube": "mango slices"})
+print(ice_cream)
