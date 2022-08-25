ff187af59d94d1fdb2c3eaf468d4733aea1f6623
Kirk Cedrick Cartaño <kirkcedrickscartano@iskolarngbayan.pup.edu.ph>
2022-08-25 16:54:17 +0800

    initial commit

--- PythonWebFlask/app1.py
+++ PythonWebFlask/app1.py
@@ -1,11 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app1 = Flask(__name__)
+
+characters = [
+    {
+        "name": "Arthurius",
+        "power": "Telepathy",
+        "weapon": "Sword",
+        "description": "Really smart but grades are pretty low"},
+    {
+        "name": "Fantasia",
+        "power": "Telekinesis",
+        "weapon": "Crossbow",
+        "description": "Shy and likes to play with racoons"},
+    {
+        "name": "Obsidian",
+        "power": "Invisibility",
+        "weapon": "Spear",
+        "description": "Silently staring at everyone"},
+
+]
+
 
 @app1.route("/")
 @app1.route("/home")
 def home():
     return "<h1>Home Page<h1>"
+    
+@app1.route("/character")
+def character():
+    return render_template("character.html", characters=characters)
+
 
 if __name__ == "__main__":
     app1.run()
