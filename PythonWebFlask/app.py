a98d3b1cbd0a09aedc383a26613d8e62426db900
Kirk Cedrick Cartaño <kirkcedrickscartano@iskolarngbayan.pup.edu.ph>
2022-09-15 11:21:25 +0800

    added sign up page

--- PythonWebFlask/app.py
+++ PythonWebFlask/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from form import SignUpForm
 
 app = Flask(__name__)
-
+app.secret_key = "secret"
 posts = [
     {"author": "Barbara Go",
      "title": "Jumping Fun!",
@@ -11,7 +12,6 @@ posts = [
      "title": "Grizzly Jaws",
      "content": "The bite of a grizzly bear is strong enough to crush a bowling ball.",
      "date_posted": "April 4, 2021"},
-
 ]
 
 
@@ -29,6 +29,15 @@ def friend():
 @app.route('/goal')
 def goal():
     return render_template("goal.html", title="Goal Page")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.is_submitted():
+        result = request.form
+        return render_template("userdata.html", result=result)
+    return render_template("signup.html", form=form)
 
 
 if __name__ == '__main__':
